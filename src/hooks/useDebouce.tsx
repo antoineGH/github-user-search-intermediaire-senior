3658048332546b80db650c8b0785a2f6f94b60ca
AntoineGH <antoine.ratat@gmail.com>
2022-10-08 14:30:19 +0200

    clean time out

--- src/hooks/useDebouce.tsx
+++ src/hooks/useDebouce.tsx
@@ -4,9 +4,11 @@ export const useDebounce = (value: string, delay: number = 2000): string => {
 	const [debounceValue, setDebounceValue] = useState(value)
 
 	useEffect(() => {
-		setTimeout(() => {
+		const debounce = setTimeout(() => {
 			setDebounceValue(value)
 		}, delay)
+
+		return () => clearTimeout(debounce)
 	}, [value, delay])
 
 	return debounceValue
